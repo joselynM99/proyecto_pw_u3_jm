053f4c4a2c255d4e0c20624da433f290e702f8c1
joselynM99 <joselynmoncayo1999@gmail.com>
2023-02-01 19:46:17 -0500

    taller25

---
+++ src/main/java/com/example/demo/ProyectPwU3Jm2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectPwU3Jm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectPwU3Jm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/ProyectPwU3Jm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectPwU3Jm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
